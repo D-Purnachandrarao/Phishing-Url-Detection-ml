beb6af952173a0ce9b94f06207c97f60df6b67d6
D-Purnachandrarao <dantalapurna@gmail.com>
2026-07-04 20:38:57 +0530

    Fix template path for Render deployment

--- PhishingDetection/settings.py
+++ PhishingDetection/settings.py
@@ -57,8 +57,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-		os.path.join('E:/venkat/2021/Jan23/Phishing/PhishingDetectionApp', 'templates'),
-	],
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
